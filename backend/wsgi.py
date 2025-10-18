42b564c77807e4f4251b3601040f8f024c4cfb90
Akshat Kaushik <iakshatkaushik@gmail.com>
2025-10-19 00:44:51 +0530

    Fix import in wsgi.py for Render deployment

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,5 +1,5 @@
 # wsgi.py
-from app import app  # Import the Flask app object from app.py
+from .app import app  # Import the Flask app object from app.py
 
 if __name__ == "__main__":
     app.run()
